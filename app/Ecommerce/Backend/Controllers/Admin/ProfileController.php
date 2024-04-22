0f27c2f10413406cec95f66811d9c47cba86a019
Muhammed2024Salama <devmuhammedsalama@gmail.com>
2024-04-22 20:15:54 +0200

    Upload Image | Admin Profile Update

--- app/Ecommerce/Backend/Controllers/Admin/ProfileController.php
+++ app/Ecommerce/Backend/Controllers/Admin/ProfileController.php
@@ -5,6 +5,7 @@ namespace Ecommerce\Backend\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 
 class ProfileController extends Controller
 {
@@ -24,9 +25,26 @@ class ProfileController extends Controller
     {
         $request->validate([
             'name' => ['required','max:100'],
-            'email' => ['required','email' , 'unique:users,email,'.Auth::user()->id]
+            'email' => ['required','email' , 'unique:users,email,'.Auth::user()->id],
+            'image' => ['image','max:2048']
         ]);
+
         $user = Auth::user();
+
+        if ($request->hasFile('image')) {
+            /** Check File If Exists Or Not If Exists Delete Old  */
+            if (File::exists(public_path($user->image))) {
+                File::delete(public_path($user->image));
+            }
+
+            $image = $request->image;
+            $imageName = rand()."_".$image->getClientOriginalName();
+            $image->move(public_path('uploads'),$imageName);
+
+            $path = "/uploads/".$imageName;
+            $user->image = $path;
+        }
+
         $user->name = $request->name;
         $user->email = $request->email;
 
--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -15,32 +15,37 @@
 
                 <div class="col-12 col-md-12 col-lg-7">
                     <div class="card">
-                        <form method="post" class="needs-validation" novalidate="" action="{{route('admin.profile.update')}}">
+                        <form method="post" action="{{ route('admin.profile.update') }}" class="needs-validation" novalidate enctype="multipart/form-data">
                             @csrf
                             <div class="card-header">
                                 <h4>Update Profile</h4>
                             </div>
                             <div class="card-body">
                                 <div class="row">
-
-                                    <div class="form-group col-md-6 col-12">
-                                        <label>Name</label>
-                                        <input type="text" name="name" class="form-control" value="{{Auth::user()->name}}">
-
+                                    <div class="form-group col-12">
+                                        <div class="mb-3">
+                                            <img width="100px" src="{{ asset(Auth::user()->image) }}"
+                                        </div>
+                                        <label for="image">Image</label>
+                                        <input type="file" name="image" id="image" class="form-control">
                                     </div>
-                                    <div class="form-group col-md-6 col-12">
-                                        <label>E-mail</label>
-                                        <input type="text" name="email" class="form-control" value="{{Auth::user()->email}}" >
 
+                                    <div class="form-group col-md-6 col-12">
+                                        <label for="name">Name</label>
+                                        <input type="text" name="name" id="name" class="form-control" value="{{ Auth::user()->name }}" required>
+                                    </div>
+
+                                    <div class="form-group col-md-6 col-12">
+                                        <label for="email">E-mail</label>
+                                        <input type="email" name="email" id="email" class="form-control" value="{{ Auth::user()->email }}" required>
                                     </div>
                                 </div>
-
-
                             </div>
                             <div class="card-footer text-right">
-                                <button class="btn btn-primary">Save Changes</button>
+                                <button type="submit" class="btn btn-primary">Save Changes</button>
                             </div>
                         </form>
+
                     </div>
                 </div>
 @endsection
