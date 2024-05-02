946a296003d063efefbb62bfbeaa630c22f43eb6
Muhammed2024Salama <devmuhammedsalama@gmail.com>
2024-05-02 14:31:27 +0300

    Category | Create Form Category

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -4,7 +4,7 @@
     <!-- Main Content -->
     <section class="section">
         <div class="section-header">
-            <h1>Category</h1>
+            <h1>All Categories</h1>
         </div>
 
         <div class="section-body">
--- resources/views/admin/layouts/script.blade.php
+++ resources/views/admin/layouts/script.blade.php
@@ -1,46 +1,44 @@
 <!-- General JS Scripts -->
-<script src="{{ asset('Backend/assets/modules/jquery.min.js') }}"></script>
-<script src="{{ asset('Backend/assets/modules/popper.js') }}"></script>
-<script src="{{ asset('Backend/assets/modules/tooltip.js') }}"></script>
-<script src="{{ asset('Backend/assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('Backend/assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
-<script src="{{ asset('Backend/assets/modules/moment.min.js') }}"></script>
-<script src="{{ asset('Backend/assets/js/stisla.j') }}s"></script>
+<script src="{{asset('backend/assets/modules/jquery.min.js')}}"></script>
+<script src="{{asset('backend/assets/modules/popper.js')}}"></script>
+<script src="{{asset('backend/assets/modules/tooltip.js')}}"></script>
+<script src="{{asset('backend/assets/modules/bootstrap/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('backend/assets/modules/nicescroll/jquery.nicescroll.min.js')}}"></script>
+<script src="{{asset('backend/assets/modules/moment.min.js')}}"></script>
+<script src="{{asset('backend/assets/js/stisla.js')}}"></script>
 
 <!-- JS Libraies -->
-<script src="{{ asset('Backend/assets/modules/simple-weather/jquery.simpleWeather.min.j') }}s"></script>
-<script src="{{ asset('Backend/assets/modules/chart.min.js') }}"></script>
-<script src="{{ asset('Backend/assets/modules/jqvmap/dist/jquery.vmap.min.js') }}"></script>
-<script src="{{ asset('Backend/assets/modules/jqvmap/dist/maps/jquery.vmap.world.js') }}"></script>
-<script src="{{ asset('Backend/assets/modules/summernote/summernote-bs4.js') }}"></script>
-<script src="{{ asset('Backend/assets/modules/chocolat/dist/js/jquery.chocolat.min.js') }}"></script>
-<script  src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-{{-- Start JS yajra DataTable https://datatables.net/ --}}
-<script src="//cdn.datatables.net/2.0.5/js/dataTables.min.js"></script>
-{{-- End JS yajra DataTable https://datatables.net/ --}}
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+<script src="{{asset('backend/assets/modules/simple-weather/jquery.simpleWeather.min.js')}}"></script>
+{{-- <script src="{{asset('backend/assets/modules/chart.min.js')}}"></script> --}}
+<script src="{{asset('backend/assets/modules/jqvmap/dist/jquery.vmap.min.js')}}"></script>
+<script src="{{asset('backend/assets/modules/jqvmap/dist/maps/jquery.vmap.world.js')}}"></script>
+<script src="{{asset('backend/assets/modules/summernote/summernote-bs4.js')}}"></script>
+<script src="{{asset('backend/assets/modules/chocolat/dist/js/jquery.chocolat.min.js')}}"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+<script src="//cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="{{asset('backend/assets/js/bootstrap-iconpicker.bundle.min.js')}}"></script>
+<script src="{{asset('backend/assets/modules/bootstrap-daterangepicker/daterangepicker.js')}}"></script>
+<script src="{{asset('backend/assets/modules/select2/dist/js/select2.full.min.js')}}"></script>
 
 <!-- Page Specific JS File -->
-<script src="{{ asset('Backend/assets/js/page/index-0.js') }}"></script>
+{{-- <script src="{{asset('backend/assets/js/page/index-0.js')}}"></script> --}}
 
 <!-- Template JS File -->
-<script src="{{ asset('Backend/assets/js/scripts.js') }}"></script>
-<script src="{{ asset('Backend/assets/js/custom.js') }}"></script>
+<script src="{{asset('backend/assets/js/scripts.js')}}"></script>
+<script src="{{asset('backend/assets/js/custom.js')}}"></script>
 
-{{--Validation Script By Toastr --}}
 <script>
-    @if($errors->any())
-        @foreach($errors->all() as $error)
-            @php
-                toastr()->error($error)
-            @endphp
-        @endforeach
+    @if ($errors->any())
+    @foreach ($errors->all() as $error)
+    toastr.error("{{$error}}")
+    @endforeach
     @endif
 </script>
 
-{{--Dynamic Delete alert --}}
+<!-- Dynamic Delete alart -->
+
 <script>
     $(document).ready(function(){
 
@@ -97,10 +95,7 @@
         })
 
     })
-
 </script>
 
 
-
 @stack('scripts')
-{{-- End Validation Script By Toastr --}}
