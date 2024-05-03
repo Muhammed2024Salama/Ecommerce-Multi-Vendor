7f4a95681a0cb3959b045289f1989f59fdedda09
Muhammed2024Salama <devmuhammedsalama@gmail.com>
2024-05-04 00:54:18 +0300

    ChildCategory | Change Status

--- app/Ecommerce/Backend/Controllers/Admin/ChildCategory/Controllers/ChildCategoryController.php
+++ app/Ecommerce/Backend/Controllers/Admin/ChildCategory/Controllers/ChildCategoryController.php
@@ -128,4 +128,17 @@ class ChildCategoryController extends Controller
 
         return response(['status' => 'success' , 'message' => 'Deleted Successfully !' ]);
     }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Foundation\Application|\Illuminate\Http\Response
+     */
+    public function changeStatus(Request $request)
+    {
+        $category = ChildCategory::findOrFail($request->id);
+        $category->status = $request->status == 'true' ? 1 : 0;
+        $category->save();
+
+        return response(['message' => 'Status has been updated!']);
+    }
 }
