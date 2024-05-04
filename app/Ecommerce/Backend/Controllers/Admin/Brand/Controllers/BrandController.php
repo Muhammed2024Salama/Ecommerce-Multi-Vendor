02099f647698d4f9fdb90dcc4b73fc7e30dccdf4
Muhammed2024Salama <devmuhammedsalama@gmail.com>
2024-05-05 02:48:09 +0300

    Brand | Creating Feature

--- app/Ecommerce/Backend/Controllers/Admin/Brand/Controllers/BrandController.php
+++ app/Ecommerce/Backend/Controllers/Admin/Brand/Controllers/BrandController.php
@@ -4,10 +4,14 @@ namespace Ecommerce\Backend\Controllers\Admin\Brand\Controllers;
 
 use App\DataTables\BrandDataTable;
 use App\Http\Controllers\Controller;
+use Ecommerce\Backend\Controllers\Admin\Brand\Models\Brand;
+use Ecommerce\Base\Traits\ImageUploadTrait;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class BrandController extends Controller
 {
+    use ImageUploadTrait;
     /**
      * Display a listing of the resource.
      */
@@ -22,7 +26,7 @@ class BrandController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.brand.create');
     }
 
     /**
@@ -30,7 +34,28 @@ class BrandController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // dd($request->all());
+
+        $request->validate([
+            'logo' => ['image' , 'required' , 'max:2000'],
+            'name' => ['required' , 'max:200'],
+            'is_featured' => ['required'],
+            'status' => ['required']
+        ]);
+
+        $logoPath = $this->uploadImage($request , 'logo' , 'uploads');
+        $brand = new Brand();
+        $brand->logo = $logoPath;
+        $brand->name = $request->name;
+        $brand->slug = Str::slug($request->name);
+        $brand->is_featured = $request->is_featured;
+        $brand->status = $request->status;
+
+        $brand->save();
+
+        toastr('Created Successfully ! ' , 'success');
+        return redirect()->route('admin.brand.index');
+
     }
 
     /**
